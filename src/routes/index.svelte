0b95389d7345353aa8563d7f6c14638633ac8d35
Fredrik Edgren <fredrik.edgren@svt.se>
2020-12-07 17:30:03 +0100

    no kids

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+
 </script>
 
 <style>
@@ -19,11 +19,6 @@
 		margin: 0 0 1em 0;
 	}
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
 
 	p {
 		margin: 1em auto;
@@ -40,10 +35,10 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>No kids!</h1>
 
 <figure>
-	<img alt="Success Kid" src="{successkid}">
+
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
